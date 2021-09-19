1d136dee4f5364c2e96fd317b18231d3cd1cea96
YTBJero <mw.sevice@gmail.com>
2021-09-19 10:24:17 +0700

    version 0.0.1

---
+++ src/YTBJero/PMVNGVipJoin/Main.php
@@ -0,0 +1,36 @@
+<?php 
+
+declare(strict_types=1);
+
+namespace YTBJero\PMVNGVipJoin;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\command\{Command, CommandSender};
+use pocketmine\Player;
+use pocketmine\Server;
+class Main extends PluginBase implements Listener{
+
+	public function onEnable(): void 
+	{
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		$this->pprs = $this->getServer()->getPluginManager()->getPlugin("PurePerms");
+		$this->saveDefaultConfig();
+		if($this->pprs == null){
+			$this->getLogger()->error("Download the pureperms plugin at https://poggit.pmmp.io/p/PurePerms and try again.");
+			$this->getServer()->getPluginManager()->disablePlugin($this);
+		}
+	}
+
+	public function onJoin(PlayerJoinEvent $event)
+	{
+		$player = $event->getPlayer();
+		foreach ($this->getConfig()->getNested("groups") as $rank) {
+			if($this->pprs->getUserDataMgr()->getGroup($player)->getName() == $rank)
+			{
+			$player->sendMessage("§l§1『 §bWelcome vip §f". $player->getName() ." §bown rank §f". $rank ." §bjoined the server.§1 』");
+		    }
+		}
+	}
+}
